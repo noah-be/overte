f2f25f206e03ee8b6a4a172c5f971e432ff139bc
noah-be <10984564+noah-be@users.noreply.github.com>
2026-08-08 16:35:23 +0200

    Fix Android tablet Home navigation

--- scripts/system/quickGoto.js
+++ scripts/system/quickGoto.js
@@ -10,27 +10,38 @@
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
-/* globals Tablet, Toolbars, Script, HMD, DialogsManager */
+/* globals Tablet, Script, Window, location, LocationBookmarks */
 
 (function() { // BEGIN LOCAL_SCOPE
 
-    function addGotoButton(name, destination) {
+    function addGotoButton(name, navigate) {
         var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
         var button = tablet.addButton({
             icon: "icons/tablet-icons/goto-i.svg",
             activeIcon: "icons/tablet-icons/goto-a.svg",
             text: name
         });
-        var buttonDestination = destination;
         button.clicked.connect(function() {
-            Window.location = buttonDestination;
+            if (typeof tablet.hideAndroidTablet === "function") {
+                tablet.hideAndroidTablet();
+            }
+            navigate();
         });
         Script.scriptEnding.connect(function () {
             tablet.removeButton(button);
         });
     }
 
-    addGotoButton("Tutorial", "file:///~/serverless/tutorial.json");
-    addGotoButton("Home", "file:///~/serverless/pico-debug.json");
+    addGotoButton("Tutorial", function () {
+        Window.location = "file:///~/serverless/tutorial.json";
+    });
+    addGotoButton("Home", function () {
+        var home = LocationBookmarks.getHomeLocationAddress();
+        if (home) {
+            location.handleLookupString(home);
+        } else {
+            Window.location = "file:///~/serverless/tutorial.json";
+        }
+    });
 
 }()); // END LOCAL_SCOPE
