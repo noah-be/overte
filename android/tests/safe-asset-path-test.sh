1d21bcd649b6cf50fefe94333bd243cd7ca2cccc
noah-be <10984564+noah-be@users.noreply.github.com>
2026-08-09 19:47:51 +0200

    test(android): isolate asset Java temp files

--- android/tests/safe-asset-path-test.sh
+++ android/tests/safe-asset-path-test.sh
@@ -5,6 +5,8 @@ readonly android_root="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.." && pwd)"
 readonly output="$(mktemp -d "${TMPDIR:-/tmp}/overte-safe-assets.XXXXXXXX")"
 trap 'rm -rf -- "$output"' EXIT
 readonly hifi_utils="$android_root/libraries/qt/src/main/java/io/highfidelity/utils/HifiUtils.java"
+readonly java_tmp="$output/java-tmp"
+mkdir -p -- "$java_tmp"
 
 grep -q 'AssetCacheExtractor.unpack(assetManager::open, destDir)' "$hifi_utils"
 grep -q 'throw new RuntimeException(e)' "$hifi_utils"
@@ -14,5 +16,5 @@ javac -d "$output" \
     "$android_root/libraries/qt/src/main/java/io/highfidelity/utils/AssetCacheExtractor.java" \
     "$android_root/tests/java/io/highfidelity/utils/AssetCacheExtractorStandaloneTest.java" \
     "$android_root/tests/java/io/highfidelity/utils/SafeAssetPathStandaloneTest.java"
-java -cp "$output" io.highfidelity.utils.SafeAssetPathStandaloneTest
-java -cp "$output" io.highfidelity.utils.AssetCacheExtractorStandaloneTest
+java -Djava.io.tmpdir="$java_tmp" -cp "$output" io.highfidelity.utils.SafeAssetPathStandaloneTest
+java -Djava.io.tmpdir="$java_tmp" -cp "$output" io.highfidelity.utils.AssetCacheExtractorStandaloneTest
